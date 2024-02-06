72bd371087e2a8b7975e748271185f8c1b860cf3
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-02-06 17:35:40 +0000

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "/tacos", :controller => "tacos", :action => "index"
   get "/dice", :controller => "dice", :action => "index"
+resources "companies"
+resources "contacts"
+#^get us "/companies", :controller => "companies", :action => "index"
 
 end
 
